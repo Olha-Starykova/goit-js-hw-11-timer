40ca90666401ac9007d922abfadeb4de9c8ff30c
Olha-Starykova <olenkalog@gmail.com>
2021-02-05 04:44:53 +0200

    com

--- js/timer.js
+++ js/timer.js
@@ -49,22 +49,22 @@ class CountdownTimer {
 
   constructor({ selector, targetDate }) {
     this.template = `<div class="field">
-            <span class="value" data-value="days">11</span>
+            <span class="value" data-value="days">00</span>
             <span class="label">Days</span>
         </div>
     
         <div class="field">
-            <span class="value" data-value="hours">11</span>
+            <span class="value" data-value="hours">00</span>
             <span class="label">Hours</span>
         </div>
     
         <div class="field">
-            <span class="value" data-value="mins">11</span>
+            <span class="value" data-value="mins">00</span>
             <span class="label">Minutes</span>
         </div>
     
         <div class="field">
-            <span class="value" data-value="secs">11</span>
+            <span class="value" data-value="secs">00</span>
             <span class="label">Seconds</span>
         </div>`
     this.selector = selector;
